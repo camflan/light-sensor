9f814bd5ad4cd9dc7174edb03bff4fc0c47b228f
Camron Flanders <camron.flanders@gmail.com>
2018-10-25 22:54:44 -0500

    insert hook code

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-'use strict';
-let { useState } = require('react');
+'use strict'
+let { useState } = require('react')
 
-function use...() {
-  return;
+function useAmbientLightSensor() {
+    let [illuminance, setIlluminance] = useState(null)
+
+    function handleIlluminanceChange(sensor) {
+        setIlluminance(sensor.illuminance)
+    }
+
+    if ('AmbientLightSensor' in window) {
+        const sensor = new AmbientLightSensor()
+        sensor.onreading = handleIlluminanceChange
+
+        sensor.onerror = event => {
+            throw new Error(event)
+        }
+
+        sensor.start()
+    } else {
+        throw new Error(
+            'This device does not support access to AmbientLightSensor.'
+        )
+    }
+
+    return illuminance
 }
 
-module.exports = use...;
+module.exports = useAmbientLightSensor
